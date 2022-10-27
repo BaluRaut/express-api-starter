dea57e883bfb9ac52fbf4f4e19fc90db942a9dfa
Balasaheb <balsraut@gmail.com>
2022-10-27 02:39:06 +0000

    Fixed API

--- src/api/index.js
+++ src/api/index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 
 const emojis = require('./emojis');
-
+var turf = require('@turf/boolean-intersects');
+const geof = require("../foo.");
 const router = express.Router();
 
 router.get('/', (req, res) => {
   res.json({
     message: 'APIok   okokokokkokoko - 👋🌎🌍🌏',
+    record: geof.features[0]
   });
 });
 
