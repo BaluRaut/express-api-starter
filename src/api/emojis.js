33c443d9036915a7a8450c0304ce4f67e8088ade
Balasaheb <balsraut@gmail.com>
2022-10-27 01:56:59 +0000

    Ok sir

--- src/api/emojis.js
+++ src/api/emojis.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.json(['😀', '😳', '🙄']);
+  res.json(['😀', '😳', 'lllllllllllllllllllllllllllllllllllllllllllllllll']);
 });
 
 module.exports = router;
